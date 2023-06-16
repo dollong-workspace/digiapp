16c163a90f05700afc341a55b72c61eb751a75c2
Doltons Edward Nicholas <edwarddoltons@gmail.com>
2023-06-16 09:16:19 +0800

    feat: setup digimons data

---
+++ src/core/db.ts
@@ -0,0 +1,27 @@
+export const DIGIMONS_DATA = [
+  {
+    name: "Koromon",
+    img: "https://digimon.shadowsmith.com/img/koromon.jpg",
+    level: "In Training",
+  },
+  {
+    name: "Tsunomon",
+    img: "https://digimon.shadowsmith.com/img/tsunomon.jpg",
+    level: "In Training",
+  },
+  {
+    name: "Yokomon",
+    img: "https://digimon.shadowsmith.com/img/yokomon.jpg",
+    level: "In Training",
+  },
+  {
+    name: "Motimon",
+    img: "https://digimon.shadowsmith.com/img/motimon.jpg",
+    level: "In Training",
+  },
+  {
+    name: "Tanemon",
+    img: "https://digimon.shadowsmith.com/img/tanemon.jpg",
+    level: "In Training",
+  },
+];
