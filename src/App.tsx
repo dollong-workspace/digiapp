4f0f34fd3f28ba665837aab86392804acb6ccc80
Doltons Edward Nicholas <edwarddoltons@gmail.com>
2023-06-17 14:05:41 +0800

    init

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,9 @@ import "./App.css";
 
 function App() {
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </>
+    <div>
+      <p>Halo, </p>
+    </div>
   );
 }
 
